103b5cb513c28fd6395114b6cecd74c23d51e201
Jefferson Souza <jrsouza@ufu.br>
2018-10-16 22:33:59 -0300

    Finished fit and evaluate

--- src/ConvLSTM.py
+++ src/ConvLSTM.py
@@ -4,8 +4,8 @@ import numpy as np
 
 class ConvLSTM():
     def __init__(self, num_classes, num_lstm_cells=128, num_lstm_layers=1,
-                 cnn_filters=(3), pool_size=3, num_cnn_cells=128,
-                 num_cnn_layers=2, dropout_rate=0.2):
+                 cnn_filters=(1), pool_size=(2), num_cnn_cells=128,
+                 num_cnn_layers=2, dropout_rate=0.4):
         self.num_classes = num_classes
         self.num_lstm_cells = num_lstm_cells
         self.num_lstm_layers = num_cnn_layers
@@ -31,11 +31,11 @@ class ConvLSTM():
 
     def create_lstm_model(self, input_shape):
         model = tf.keras.models.Sequential()
-        model.add(tf.keras.layers.LSTM(self.num_lstm_cells,
+        model.add(tf.keras.layers.CuDNNLSTM(self.num_lstm_cells,
                   input_shape=input_shape, return_sequences=True))
         model.add(tf.keras.layers.Dropout(self.dropout_rate))
         for layer in range(1, self.num_lstm_layers):
-            model.add(tf.keras.layers.LSTM(self.num_lstm_cells))
+            model.add(tf.keras.layers.CuDNNLSTM(self.num_lstm_cells))
             model.add(tf.keras.layers.Dropout(self.dropout_rate))
         return model
 
@@ -54,23 +54,22 @@ class ConvLSTM():
                                                activation=tf.nn.softmax,
                                                name='network_output'
                                                )(network_output)
-        print(network_output.get_shape())
         model = tf.keras.models.Model(inputs=[lstm_input, cnn_input],
                                       outputs=[network_output])
-        model.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
+        model.compile(optimizer='adam', loss='categorical_crossentropy',
+                      metrics=['accuracy'])
         self.model = model
 
     def fit(self, input_data, labels, num_epochs, time_steps, num_features,
             batch_size, learn_rate=0.01):
         cnn_input = tf.reshape(input_data, (-1, time_steps, num_features))
         lstm_input = tf.reshape(input_data, (-1, time_steps, num_features))
-        labels = tf.reshape(labels, (-1, self.num_classes))
-        print(labels.shape)
         self.model.fit({'lstm_input': lstm_input, 'cnn_input': cnn_input},
                        {'network_output': labels},
                        epochs=num_epochs, steps_per_epoch=batch_size)
 
-    def evaluate(self, test_data, test_labels):
-        loss, accuracy = self.model.evaluate(test_data, test_labels)
+    def evaluate(self, test_data, test_labels, time_steps, num_features):
+        test_data = tf.reshape(test_data, (-1, time_steps, num_features))
+        loss, accuracy = self.model.evaluate(x=[test_data, test_data], y=test_labels, steps=2)
         print("Model loss:", loss, ", Accuracy:", accuracy)
         return loss, accuracy
--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import numpy as np
 from ConvLSTM import ConvLSTM
 import data_handler as data
 import tensorflow as tf
+import os
 
 
 if __name__ == '__main__':
@@ -12,6 +13,7 @@ if __name__ == '__main__':
     timesteps = 150
     rnn_size = 256
     max_len = 150
+    os.environ['CUDA_VISIBLE_DEVICES'] = str(1)
     network_input = tf.placeholder(tf.float32, [None, timesteps, num_features])
     network_output = tf.placeholder('float', [None, n_classes])
     inputs, labels = data.open_data(max_len=max_len)
@@ -26,3 +28,4 @@ if __name__ == '__main__':
     print(inputs.dtype)
     model.create_network(network_input, timesteps, num_features)
     model.fit(inputs, labels, num_epochs, timesteps, num_features, batch_size)
+    model.evaluate(test_inputs, test_labels, timesteps, num_features)
